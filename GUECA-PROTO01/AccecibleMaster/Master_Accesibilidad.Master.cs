75bfe85d69689c5ae68a32811537be6ab59a6c4c
EspinozaHome <michaelespinozabetanco@gmail.com>
2020-11-24 15:50:43 -0600

    Primer committe, archivos basicos

---
+++ GUECA-PROTO01/AccecibleMaster/Master_Accesibilidad.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GUECA_PROTO01.AccecibleMaster
+{
+    public partial class Master_Accesibilidad : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
